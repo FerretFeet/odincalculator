0ccf7e39aa397e0816b87b6e1789eff209e0cdb0
Brandon <73044701+FerretFeet@users.noreply.github.com>
2023-04-14 21:28:18 -0500

    added DivideByZero alert and rounding and comments. still room for improvement w/ refactor but done for now

--- script.js
+++ script.js
@@ -10,8 +10,13 @@ const subtract = function(x,y) {
 const multiply = function(x,y) {
     return x * y;
 }
-//divide 2 numbers
+//divide 2 numbers and alert if divideByZero
 const divide = function(x,y) {
+    if (y == 0) {
+        alert('0 chance of that happening(divideByZero)')
+     
+        return;
+    }
     return x / y;
 }
 //create global variables
@@ -33,22 +38,24 @@ const operate = function(x, operation, y) {
             return divide(x,y)
     }
 }
-
+//grab classlist for number buttons, iterate to create event listener
 const createNumberButtons = function() {
     let buttons = document.getElementsByClassName('number-button');
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', () => {
+//keep display string within display size, concat value of buttonpress to displayString
             if (displayString.length > 11) {alert('Too Many Digits! Press ='); return;}
             displayString = displayString.concat(buttons[i].textContent)
             displayResult(displayString);
         })
     }
 }
-
+//see above
 const createOperatorButtons = function() {
     let buttons = document.getElementsByClassName('operator-button');
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', () => {
+//see above + ensure no 2 operators in a row. replace previous ending operator with new if attempted.
             if (displayString.length > 11) {alert('Too Many Digits! Press ='); return;}
             let regEx = /[+x/-]+$/;
             let tempString = displayString;
@@ -66,7 +73,7 @@ function createAllButtons() {
     createNumberButtons();
     createOperatorButtons();
 }
-
+//split the string before and after the items inside the reg ex. no loss.
 const arrayFromString = function(String) {
     let regEx = /([+x/-])/g;
     return String.split(regEx)
@@ -75,19 +82,34 @@ const arrayFromString = function(String) {
 
 createAllButtons()
 
+//compute answer by splitting the formula 3 peices at a time.
+//adds rounded answer to beginning of array to iterate again until complete
+
 const computeAnswer = function(Array) {
     let answer = Array[0];
     let tempArray = Array
     let splicedArray = []
     while(Array.length > 1) {
         splicedArray = tempArray.splice(0, 3)
-        answer = operate(splicedArray[0], splicedArray[1], splicedArray[2])
-        Array.unshift(answer)
+        answer = (operate(splicedArray[0], splicedArray[1], splicedArray[2]))
+        //if answer has >3 digits after decimal, round
+        if ((answer*10000) % 10 != 0 ) {
+            answer = Number(answer).toFixed(3)
+        }
+        let test = Number.isNaN(Number(answer))
+        if (!test) {
+            Array.unshift(answer)
+        } else {
+            answer = 0; 
+            displayString = ''; 
+            break;
+        }
     }
     displayResult(answer)
     return answer;
 }
 
+//run functions to evaluate
 const onEqualsPress = function(String) {
     let tempArray = arrayFromString(String);
     return computeAnswer(tempArray);
@@ -96,6 +118,10 @@ const onEqualsPress = function(String) {
 let equals = document.getElementById('equals');
 equals.addEventListener('click', () => displayString=String(onEqualsPress(displayString)))
 
+//hide default display, show displayString
+//might could be optimized by just replacing default display string and recalling when clr
+//shows result and every digit/operator pressed.  
+//could be changed to only display working term be appending arrays, breaking before/after op button press
 const resultDisplay = document.getElementById('display-result')
 const displayResult = function(result) {
     if (resultDisplay.style.display == '' || 'none') {
@@ -105,6 +131,7 @@ const displayResult = function(result) {
     resultDisplay.innerHTML = result;
 }
 
+//clear working vars, show default display, hide result display
 const clearButton = document.getElementById('clear')
 clearButton.addEventListener('click', () => {
     displayString = ''
@@ -112,6 +139,9 @@ clearButton.addEventListener('click', () => {
     document.getElementById('display-default').style.display='flex'
 })
 
+//ensure only 1 decimal per numeric term.
+//adds all decimal presses to string, then evaluates if error
+//creates a new string of size oldString - 1 to remove last placed decimal.
 const decimalButton = document.getElementById('decimal')
 decimalButton.addEventListener('click', () => {
     if (displayString.length > 11) {alert('Too Many Digits! Press ='); return}
